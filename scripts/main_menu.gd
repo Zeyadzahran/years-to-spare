527a9a4d25093c4ef5757b79c94659ed005dd0af
Ahmed Elaidy <elaidya225@gmail.com>
2026-09-02 17:42:16 +0300

    add music for the intro and the main menu and fade in transitions

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var menu_music = preload("res://assets/music/The_Weight_of_November.mp3")
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file(
@@ -13,3 +14,6 @@ func _on_options_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _ready():
+	MusicManager.play_music(menu_music, 1.5)
