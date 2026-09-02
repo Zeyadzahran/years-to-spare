9adf17f4ab3b2ea45a4e974df27b451e17a5e3b0
Ahmed Elaidy <elaidya225@gmail.com>
2026-09-02 15:38:20 +0300

    add level intro and name before the game strats

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -94,7 +94,7 @@ func play_scene(index: int):
 	await fade_out()
 
 func start_game():
-	get_tree().change_scene_to_file("res://src/levels/level_01.tscn")
+	get_tree().change_scene_to_file("res://src/levels/garbage_eden.tscn")
 
 func show_title():
 	$Title.modulate.a = 0.0
